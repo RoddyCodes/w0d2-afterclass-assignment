863c4ad4a91610b4ce146f5695462327023402b9
roddy2022 <141802073+RoddyCodes@users.noreply.github.com>
2024-09-17 21:59:14 -0400

    added greet person test

--- test_main.py
+++ test_main.py
@@ -1,12 +1,14 @@
 # test_main.py
 import unittest
 from main import hello_world
+from main import greet_person
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(hello_world(), "Hello from Ryan Pham!")
+class TestGreetPerson(unittest.TestCase):
     def greet_person(name):
-        self.assertEqual(greet_person(name), "Hello, %s!" % (name))
+        name.assertEqual(greet_person(), "Hello, %s!" % (name))
 
 if __name__ == '__main__':
     unittest.main()
