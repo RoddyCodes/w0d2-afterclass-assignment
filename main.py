718745039ea220dcb33d7e30e94e9495f8ab8ce5
roddy2022 <141802073+RoddyCodes@users.noreply.github.com>
2024-09-17 21:57:30 -0400

    added greet person

--- main.py
+++ main.py
@@ -3,5 +3,7 @@
 def hello_world():
     return "Hello from Ryan Pham!"
 
+def greet_person(name):
+    return "Hello, %s!" % (name)
 if __name__ == "__main__":
     print(hello_world())
--- test_main.py
+++ test_main.py
@@ -5,6 +5,8 @@ from main import hello_world
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(hello_world(), "Hello from Ryan Pham!")
+    def greet_person(name):
+        self.assertEqual(greet_person(name), "Hello, %s!" % (name))
 
 if __name__ == '__main__':
     unittest.main()
